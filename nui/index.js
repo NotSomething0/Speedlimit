d44dc4de7cb1564cc7c52e83da4fc43e54a3f811
NotSomething0 <programming.unconsociated@aleeas.com>
2021-02-26 23:34:03 -0600

    Add assets

--- nui/index.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('message', function(event){
-    if(event.data.status == true){
-        console.log('element should be shown')
-    } else if(event.data.status == false){
-        console.log('element should be hidden')
-    }
-});
